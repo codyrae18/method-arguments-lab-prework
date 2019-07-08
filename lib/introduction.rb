a3f4b54367505185518d0b1c3290f67a91363587
Cody Lawdermilt <lawdermiltcody@gmail.com>
2019-07-08 16:42:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,4 +4,4 @@ def introduction(name)
 end
 
 def #introduction_with_language(name, language)
-  puts "Hi my name is #{Dan} and I am learning to program in #{language}"
+  puts "Hi my name is #{Dan} and I am learning to program in #{Ember.js}"
